eb9e41d11d32fe91f23714211038d3772a5ccdf9
Di <kashapova.dilyara2016@yandex.ru>
2021-07-08 19:46:40 +0300

    7.07.21
    Fixed scene lGame and create ramdom GameObject

--- DL [DML SS 2021]/Assets/LGame/Script/Road scripts/Road.cs
+++ DL [DML SS 2021]/Assets/LGame/Script/Road scripts/Road.cs
@@ -5,4 +5,36 @@ using UnityEngine;
 public class Road : MonoBehaviour
 {
     public int number;
+    public float maxPosZ;
+
+    private string statusActiveVar = "";
+    private string statusOffVar = "";
+    private int currentVariant;
+
+    public GameObject[] Variants;
+
+    private void FixedUpdate() 
+    {
+        if (transform.localPosition != new Vector3(0, 0, -40))
+        {
+            if (transform.localPosition.z < maxPosZ)
+            {
+                if (statusActiveVar == "")
+                {
+                    currentVariant = Random.Range(0, Variants.Length);
+                    Variants[currentVariant].SetActive(true);
+                    statusActiveVar = "Active";
+                    statusOffVar = "Check";
+                }
+            }
+        }
+        else {
+            if (statusOffVar == "Check")
+            {
+                Variants[currentVariant].SetActive(false);
+                statusActiveVar = "";
+                statusOffVar = "";
+            }
+        }
+    }
 }
